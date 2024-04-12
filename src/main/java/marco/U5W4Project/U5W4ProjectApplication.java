e7b49c47c8779fd9cd4be240ab34d368475a616c
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-12 10:28:50 +0200

    Initial commit

---
+++ src/main/java/marco/U5W4Project/U5W4ProjectApplication.java
@@ -0,0 +1,13 @@
+package marco.U5W4Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W4ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W4ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marco/U5W4Project/U5W4ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package marco.U5W4Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W4ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
